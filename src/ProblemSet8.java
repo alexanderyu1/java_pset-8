fee997cfae0bcf6fea634564459137a54d8b2977
Alex Yu <alex@NEBULA.milkyway.net>
2021-01-24 23:44:41 -0500

    final

--- src/ProblemSet8.java
+++ src/ProblemSet8.java
@@ -1,42 +1,137 @@
 public class ProblemSet8 {
 
     public boolean common(int[] a, int[] b) {
-//bruh
+        if (a == null || b == null || a.length == 0 || b.length == 0) {
+            return false;
+        }
+        else {
+            return (a[a.length - 1] == b[b.length - 1]) || (a[0] == b[0]);
+        }
     }
 
     public int[] middleman(int[] a, int[] b) {
-
+        if (a == null || b == null || a.length % 2 != 1 || b.length % 2 != 1) {
+            return null;
+        }
+        else {
+            return new int[] {a[a.length / 2], b[b.length / 2]};
+        }
     }
 
     public int[] bigger(int[] a, int[] b) {
-
+        int sum1 = 0;
+        int sum2 = 0;
+        if (a == null || b == null) {
+            return null;
+        }
+        else {
+            for (int i : a) {
+                sum1 += i;
+            }
+            for (int i : b) {
+                sum2 += i;
+            }
+            if (sum1 >= sum2) {
+                return a;
+            } else {
+                return b;
+            }
+        }
     }
 
     public int[] doubleMiddle(int[] a, int[] b) {
-
+        if (a == null || b == null || a.length == 0 || b.length == 0 || a.length % 2 != 0 || b.length % 2 != 0) {
+            return null;
+        }
+        else {
+            return new int[] {a[a.length/2 - 1], a[a.length/2], b[b.length/2 - 1], b[b.length/2]};
+        }
     }
 
     public int[] swapMe(int[] a) {
-
+        if (a == null) {
+            return null;
+        }
+        else {
+            int n = a[a.length - 1];
+            a[a.length - 1] = a[0];
+            a[0] = n;
+            return a;
+        }
     }
 
     public int[] threePoint(int[] a) {
-
+        if (a == null || a.length % 2 != 1) {
+            return null;
+        }
+        else {
+            return new int[] {a[0], a[a.length / 2], a[a.length - 1]};
+        }
     }
 
     public int headHoncho(int[] a) {
-
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+        else {
+            int largest = 0;
+            for (int i : a) {
+                if (i < 0) {
+                    return -1;
+                } else if (i > largest) {
+                    largest = i;
+                }
+            }
+            return largest;
+        }
     }
 
     public boolean tippingPoint(int[] a, int threshold) {
-
+        if (a == null) {
+            return false;
+        }
+        else {
+            int array_sum = 0;
+            for (int i : a) {
+                array_sum += i;
+            }
+            return array_sum > threshold;
+        }
     }
 
-    public String halfway(int[] a) {
-
+    public static String halfway(int[] a) {
+        if (a == null || a.length == 0 || a.length % 2 != 0) {
+            return null;
+        }
+        int left_sum = 0;
+        int right_sum = 0;
+        for (int m = 0; m < a.length/2; m++) {
+            left_sum += a[m];
+        }
+        for (int n = (a.length/2); n < a.length; n++) {
+            right_sum += a[n];
+        }
+        if (left_sum > right_sum) {
+            return "LEFT";
+        }
+        else {
+            return "RIGHT";
+        }
     }
 
     public int[] sequential(int[] a, int[] b) {
-
+        if (a == null || b == null || (a.length + b.length < 3)) {
+            return null;
+        }
+        else {
+            int[] answer = new int[3];
+            int length = a.length;
+            int remain = 3-length;
+            System.arraycopy(a, 0, answer, 0, length);
+            if (remain > 0) {
+                System.arraycopy(b, 0, answer, length, remain);
+            }
+            return answer;
+        }
     }
 }
